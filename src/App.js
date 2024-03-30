0756d08d9e633691ff102a9b326861f97d03e5ae
Abhay Wakle <wakleabhay2@gmail.com>
2024-03-30 23:16:30 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Cards from './components/Cards';
+import Filter from './components/Filter';
+import Navbar from './components/Navbar';
+import Spinner from './components/Spinner';
+import { filterData, apiUrl } from './data';
+import { toast } from 'react-toastify';
+
+
 
 function App() {
+
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData.title)
+
+  async function fetchData() {
+    setLoading(true);
+    try {
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+
+      setCourses(output.data)
+    }
+    catch (error) {
+      toast.error('Newtork error');
+    }
+    setLoading(false);
+  }
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='min-h-screen flex flex-col'>
+
+      <div>
+        <Navbar />
+      </div>
+
+      <div className='bg-bgDark2'>
+        <div>
+          <Filter
+            filterData={filterData}
+            category={category}
+            setCategory={setCategory} />
+        </div>
+
+        <div className='w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]'>
+          {
+            loading ? (<Spinner />) : (<Cards courses={courses}
+              category={category} />)
+          }
+
+        </div>
+      </div>
+
+
     </div>
   );
 }
